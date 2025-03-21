ddd97713dcbcbd68ffe50749886dcc3ef6e89514
mangdulgi <mangdulgi99@gmail.com>
2025-03-21 19:46:33 +0900

    웹사이트 변경 사항 업데이트

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "495137159"
+    serviceWorkerVersion: "2726173578"
   }
 });
